bfefa76996a4f6cbb62a958547c5fae74af92b5d
irvin <irvin.gy.kh@gmail.com>
2018-12-14 17:43:54 +0800

    增加結構

---
+++ database/migrations/2018_12_14_033638_create_category_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->increments('id')->comment('流水號');
+            $table->string('name')->comment('分類名稱');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
---
+++ database/migrations/2018_12_14_033709_create_article_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->increments('id')->comment('流水號');
+            $table->string('title')->comment('標題');
+            $table->string('discription')->comment('描述');
+            $table->string('content')->comment('內文');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
